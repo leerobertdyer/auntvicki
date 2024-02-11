a5cedfc9ab540aa29723746fdca293f3b91dcdb6
leerobertdyer <leerobertdyer@gmail.com>
2024-02-11 11:45:16 -0500

    fixed buttons on header for erin

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,10 +2,10 @@ import Nav from '../Nav/Nav'
 import './Header.css'
 import { SocialIcon } from 'react-social-icons'
 import { FaCameraRetro } from "react-icons/fa";
-import { FaSpotify } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
-const facebookIcon = <SocialIcon className="icon" url="https://www.facebook.com/AuntVickiBand" target="_blank"/>
-const instagramIcon = <SocialIcon className="icon" url="https://www.instagram.com/_aunt_vicki" target="_blank"/>
+const facebookIcon = <SocialIcon className="icon" url="https://www.facebook.com/AuntVickiBand" target="_blank" />
+const instagramIcon = <SocialIcon className="icon" url="https://www.instagram.com/_aunt_vicki" target="_blank" />
 
 function Header() {
 
@@ -17,20 +17,20 @@ function Header() {
                 <div className='iconAndQuoteDiv'>
                     {facebookIcon}
                     <div>
-                    <h2 className='underQuote'>Aunt<span className='spacingSpan'> </span>Vicki</h2>
-                    <h1 className='coolAunt'>...If your cool aunt was a band</h1>
+                        <h2 className='underQuote'>Aunt<span className='spacingSpan'> </span>Vicki</h2>
+                        <h1 className='coolAunt'>...If your cool aunt was a band</h1>
                     </div>
                     {instagramIcon}
                 </div>
                 <div className='headerBtnDiv'>
-                    <a href="#merch">
-                        {/* <button className='headerBtn'><span className='cheetahText'>Aunt Vicki's Shop</span></button> */}
-                    </a>
+                    <Link to="/merch">
+                        <button className='headerBtn'><span className='cheetahText'>Aunt Vicki's Shop</span></button>
+                    </Link>
                     <a href="https://spotify.auntvicki.rocks/spotify/auntvicki"
                         target="_blank">
-                        <button className='headerBtn'><div className='cheetahText'><FaSpotify size={50}/> Follow Us</div></button></a>
+                        <button className='headerBtn'><div className='cheetahText'>Follow us on Spotify</div></button></a>
                 </div>
-        <p className='photoCredit' style={{position: 'absolute'}}><FaCameraRetro/> by <a href="https://www.geddimonroe.com/" target="_blank">Geddi Monroe</a></p>
+                <p className='photoCredit' style={{ position: 'absolute' }}><FaCameraRetro /> by <a href="https://www.geddimonroe.com/" target="_blank">Geddi Monroe</a></p>
             </div>
         </div>
     )
